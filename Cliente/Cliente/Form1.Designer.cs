6318b10f0148dbf5fe4fcd07f5c80635a97881db
MarcJane <marc.jane.munoz@estudiantat.upc.edu>
2024-10-10 11:13:05 +0200

    prueba dos

---
+++ Cliente/Cliente/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Cliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.USER = new System.Windows.Forms.TextBox();
+            this.PASSWORD = new System.Windows.Forms.TextBox();
+            this.NAME = new System.Windows.Forms.TextBox();
+            this.DATE = new System.Windows.Forms.TextBox();
+            this.LOGIN = new System.Windows.Forms.Button();
+            this.SIGNIN = new System.Windows.Forms.Button();
+            this.DISCONECT = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lConectados = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USUARIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONTRASEÑA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CONSULTAS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NOMBRE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(243, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "FECHA";
+            // 
+            // USER
+            // 
+            this.USER.Location = new System.Drawing.Point(53, 42);
+            this.USER.Name = "USER";
+            this.USER.Size = new System.Drawing.Size(133, 22);
+            this.USER.TabIndex = 5;
+            // 
+            // PASSWORD
+            // 
+            this.PASSWORD.Location = new System.Drawing.Point(53, 102);
+            this.PASSWORD.Name = "PASSWORD";
+            this.PASSWORD.Size = new System.Drawing.Size(133, 22);
+            this.PASSWORD.TabIndex = 6;
+            // 
+            // NAME
+            // 
+            this.NAME.Location = new System.Drawing.Point(53, 265);
+            this.NAME.Name = "NAME";
+            this.NAME.Size = new System.Drawing.Size(133, 22);
+            this.NAME.TabIndex = 7;
+            // 
+            // DATE
+            // 
+            this.DATE.Location = new System.Drawing.Point(53, 331);
+            this.DATE.Name = "DATE";
+            this.DATE.Size = new System.Drawing.Size(133, 22);
+            this.DATE.TabIndex = 8;
+            // 
+            // LOGIN
+            // 
+            this.LOGIN.Location = new System.Drawing.Point(475, 41);
+            this.LOGIN.Name = "LOGIN";
+            this.LOGIN.Size = new System.Drawing.Size(91, 41);
+            this.LOGIN.TabIndex = 9;
+            this.LOGIN.Text = "LOG IN";
+            this.LOGIN.UseVisualStyleBackColor = true;
+            this.LOGIN.Click += new System.EventHandler(this.LOGIN_Click);
+            // 
+            // SIGNIN
+            // 
+            this.SIGNIN.Location = new System.Drawing.Point(475, 128);
+            this.SIGNIN.Name = "SIGNIN";
+            this.SIGNIN.Size = new System.Drawing.Size(91, 41);
+            this.SIGNIN.TabIndex = 10;
+            this.SIGNIN.Text = "SIGN IN";
+            this.SIGNIN.UseVisualStyleBackColor = true;
+            this.SIGNIN.Click += new System.EventHandler(this.SIGNIN_Click);
+            // 
+            // DISCONECT
+            // 
+            this.DISCONECT.Location = new System.Drawing.Point(656, 81);
+            this.DISCONECT.Name = "DISCONECT";
+            this.DISCONECT.Size = new System.Drawing.Size(120, 38);
+            this.DISCONECT.TabIndex = 11;
+            this.DISCONECT.Text = "DISCONECT";
+            this.DISCONECT.UseVisualStyleBackColor = true;
+            this.DISCONECT.Click += new System.EventHandler(this.DISCONECT_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lConectados});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lConectados
+            // 
+            this.lConectados.Name = "lConectados";
+            this.lConectados.Size = new System.Drawing.Size(101, 24);
+            this.lConectados.Text = "Conectados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DISCONECT);
+            this.Controls.Add(this.SIGNIN);
+            this.Controls.Add(this.LOGIN);
+            this.Controls.Add(this.DATE);
+            this.Controls.Add(this.NAME);
+            this.Controls.Add(this.PASSWORD);
+            this.Controls.Add(this.USER);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox USER;
+        private System.Windows.Forms.TextBox PASSWORD;
+        private System.Windows.Forms.TextBox NAME;
+        private System.Windows.Forms.TextBox DATE;
+        private System.Windows.Forms.Button LOGIN;
+        private System.Windows.Forms.Button SIGNIN;
+        private System.Windows.Forms.Button DISCONECT;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lConectados;
+    }
+}
+
---
+++ Cliente/Cliente/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        Thread atender;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void atenderServidor()
+        {
+            while (true)
+            {
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                string[] trozos = Encoding.ASCII.GetString(msg2).Split('-');
+                int codigo = Convert.ToInt32(trozos[1]);
+                string mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                switch (codigo)
+                {
+                    case 0:
+                        if (mensaje == "0-0")
+                        {
+                            MessageBox.Show("Has accedido a la cuenta");
+                            this.BackColor = Color.Green;
+                            lConectados.BackColor = Color.Green;
+                            menuStrip1.BackColor = Color.Green;
+                            label3.Visible = true;
+                            label4.Visible = true;
+                            label5.Visible = true;
+                            NAME.Visible = true;
+                            DATE.Visible = true;
+                            lConectados.Visible = true;
+                            USER.Enabled = false;
+                            PASSWORD.Enabled = false;
+                            SIGNIN.Enabled = true;
+                            LOGIN.Enabled = false;
+                        }
+                        if (mensaje == "Error")
+                        {
+                            MessageBox.Show("Usuario o contraseña incorrectos");
+                        }
+                        break;
+                    case 1:
+                        if (mensaje != "Error")
+                        {
+                            int i = 1;
+                            string jugadores = "Los jugadores que jugaron la partida mas larga son : " + mensaje.Split('-')[i];
+                            i++;
+                            while (i < mensaje.Split('-').Length)
+                            {
+                                jugadores = jugadores + " ," + mensaje.Split('-')[i];
+                                i++;
+                            }
+                            MessageBox.Show(jugadores);
+                        }
+                        if (mensaje == "Error")
+                        {
+                            MessageBox.Show("No hay datos coincidentes");
+                        }
+                        break;
+                    case 2:
+
+                        if (mensaje != "Error")
+                        {
+                            string mensaje2 = mensaje.Split('-')[1];
+                            MessageBox.Show("El jugador " + mensaje2 + " fue el jugador con mas partidas el dia " + DATE.Text);
+                        }
+                        if (mensaje == "Error")
+                        {
+                            MessageBox.Show("No hay datos coincidentes");
+                        }
+                        break;
+                    case 3:
+                        if (mensaje != "Error")
+                        {
+                            double Wins = Convert.ToDouble(mensaje.Split('-')[1]);
+                            double Played = Convert.ToDouble(mensaje.Split('-')[2]);
+                            double wr;
+                            wr = (Wins / Played) * 100;
+                            MessageBox.Show("El jugador " + NAME.Text + " tuvo un winratio de " + wr + "% el dia " + DATE.Text);
+                        }
+                        if (mensaje == "Error")
+                        {
+                            MessageBox.Show("No hay datos coincidentes");
+                        }
+                        break;
+                    case 4:
+                        if (mensaje == "4-0")
+                        {
+                            MessageBox.Show("Registrado correctamente");
+                            MessageBox.Show("Has accedido a la cuenta");
+                            this.BackColor = Color.Green;
+                            lConectados.BackColor = Color.Green;
+                            menuStrip1.BackColor = Color.Green;
+                            label3.Visible = true;
+                            label4.Visible = true;
+                            label5.Visible = true;
+                            NAME.Visible = true;
+                            DATE.Visible = true;
+                            lConectados.Visible = true;
+                            USER.Enabled = false;
+                            PASSWORD.Enabled = false;
+                            SIGNIN.Enabled = false;
+                            LOGIN.Enabled = false;
+                        }
+                        if (mensaje == "ERROR")
+                        {
+                            MessageBox.Show("Ha habido un problema con la creación de cuenta");
+                        }
+                        break;
+                    case 5:
+                        server.Shutdown(SocketShutdown.Both);
+                        this.BackColor = Color.Gray;
+                        lConectados.BackColor = Color.Gray;
+                        menuStrip1.BackColor = Color.Gray;
+                        label3.Visible = false;
+                        label4.Visible = false;
+                        label5.Visible = false;
+                        NAME.Visible = false;
+                        DATE.Visible = false;
+                        lConectados.Visible = false;
+                        USER.Enabled = true;
+                        PASSWORD.Enabled = true;
+                        SIGNIN.Enabled = true;
+                        LOGIN.Enabled = true;
+                        server.Close();
+                        MessageBox.Show("DESCONECTADO DEL SERVIDOR");
+                        break;
+                    case 6:
+                        int id = 0;
+                        lConectados.DropDownItems.Clear();
+                        foreach (String items in dameListaConectados(mensaje))
+                        {
+                            ToolStripMenuItem item = new ToolStripMenuItem(items);
+                            item.Tag = id;
+                            id++;
+                            lConectados.DropDownItems.Add(item);
+                        }
+                        break;
+                }
+            }
+        }
+        private void LOGIN_Click(object sender, EventArgs e)
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 5060);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+
+
+                string login = "0" + "-" + USER.Text + "-" + PASSWORD.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(login);
+
+                server.Send(mensaje);
+
+                ThreadStart ts = delegate { atenderServidor(); };
+                atender = new Thread(ts);
+                atender.Start();
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return;
+            }
+        }
+
+        private void SIGNIN_Click(object sender, EventArgs e)
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 5060);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                string SIGNIN1 = "4" + "-" + USER.Text + "-" + PASSWORD.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(SIGNIN1);
+
+                server.Send(mensaje);
+
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return;
+            }
+        }
+
+        private void DISCONECT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.BackColor = Color.Green;
+
+                string CERRAR = "5" + "-" + USER.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(CERRAR);
+
+                server.Send(mensaje);
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("DESCONECTADO DEL SERVIDOR");
+                return;
+            }
+            atender.Abort();
+        }
+        private List<String> dameListaConectados(string lista)
+        {
+            List<String> conectados = new List<String>();
+            string[] lconectados = lista.Split('-');
+
+            int numeroConectados = Convert.ToInt32(lconectados[1]);
+            int i = 2;
+            while (i < lconectados.Length)
+            {
+                string nombre = lconectados[i];
+                conectados.Add(nombre);
+                i++;
+            }
+            return conectados;
+        }
+    }
+}
